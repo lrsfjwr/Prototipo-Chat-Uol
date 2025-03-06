096819d6fd77f0998d1f9c57e4be24c84c7788c2
fuji <lrs.fujiwara@gmail.com>
2025-03-06 09:23:46 -0300

    Chat UOL - V3

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let mensagens = [];
+let participantes = [];
 let novoNome = prompt("Olá! Digite seu nome para entrar:");
 
+//Adicionar usuário no chat
 const adicionarNovoParticipante = () => {
+
     const novoParticipante = {
         name: novoNome
     };
@@ -9,8 +12,11 @@ const adicionarNovoParticipante = () => {
     const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/36779bab-ebca-4960-aa5c-78095c04c3f6", novoParticipante);
     promessa.then(buscarMensagens);
     promessa.catch(nomeExistente);
+    participantes.push(novoParticipante);
+
 }
 
+//informa que o nome já está em uso e pede um novo (e fica em looping até mudar)
 const nomeExistente = erro => {
     switch (erro.response.status) {
         case 400:
@@ -20,6 +26,23 @@ const nomeExistente = erro => {
     }
 }
 
+//mantém conexão do usuario enviando o objeto para a API a cada 5s.
+//chama função a cada de buscar e atualizar mensagens
+const participantesOnline = () => {
+
+    const usuariosOnline = {
+        name: novoNome
+    }
+
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/status/36779bab-ebca-4960-aa5c-78095c04c3f6", usuariosOnline);
+    promessa.then(atualizarMensagens);
+    promessa.catch(participanteOffline);
+
+}
+
+const atualizarParticipantes = () => setInterval(participantesOnline, 5000);
+
+//Faz um get na API e tras as mensagens e chama função de renderizar msgs e atualiza a cada 3s
 const buscarMensagens = () => {
     const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages/36779bab-ebca-4960-aa5c-78095c04c3f6");
     promessa.then(renderizarMensagens);
@@ -28,19 +51,20 @@ const buscarMensagens = () => {
 const renderizarMensagens = resposta => {
     mensagens = resposta.data;
 
-    const p = document.querySelector("main");
-    p.innerHTML = "";
+    const div = document.querySelector("main");
+    div.innerHTML = "";
 
     for (let i = 0; i < mensagens.length; i++) {
         if (mensagens[i].type === "message") {
-            p.innerHTML += `
+            div.innerHTML += `
                 <p class="${mensagens[i].type}">
                 ${mensagens[i].time} ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
                 <p>
             `
+
         }
         else {
-            p.innerHTML += `
+            div.innerHTML += `
                 <p class="${mensagens[i].type}">
                 ${mensagens[i].time} ${mensagens[i].from} ${mensagens[i].text}
                 <p>
@@ -50,4 +74,20 @@ const renderizarMensagens = resposta => {
 
 }
 
+const atualizarMensagens = () => setInterval(buscarMensagens, 3000);
+
+
+
+const participanteOffline = erro => {
+    console.log(erro);
+}
+
+
+/* const buscarParticipantes = () => {
+    const 
+} */
+
+//funções que inicializam quando abre a página
 adicionarNovoParticipante();
+atualizarParticipantes();
+
