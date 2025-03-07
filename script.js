799235d16a296724a3940d67f71c88fbb991b558
fuji <lrs.fujiwara@gmail.com>
2025-03-06 21:32:42 -0300

    Desenvolvimento V3

--- script.js
+++ script.js
@@ -1,93 +1,60 @@
-let mensagens = [];
-let participantes = [];
-let novoNome = prompt("Olá! Digite seu nome para entrar:");
+let nomeParticipante = prompt("Olá! Digite seu nome para entrar:");
 
-//Adicionar usuário no chat
-const adicionarNovoParticipante = () => {
-
+//Participante entra na sala, e pede-se um nome:
+const adicionarParticipante = () => {
     const novoParticipante = {
-        name: novoNome
-    };
+        name: nomeParticipante
+    }
 
-    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/36779bab-ebca-4960-aa5c-78095c04c3f6", novoParticipante);
-    promessa.then(buscarMensagens);
+    const promessa = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/participants/2859eeff-8821-45c5-ab78-67cda96705d2",
+        novoParticipante);
+    promessa.then(logarNoChat);
     promessa.catch(nomeExistente);
-    participantes.push(novoParticipante);
-
 }
 
-//informa que o nome já está em uso e pede um novo (e fica em looping até mudar)
+//Em caso de sucesso ao inserir nome
+const logarNoChat = resposta => {
+    const atualizaStatusParticipante = setInterval(enviarStatus, 5000);
+    const atualizaMensagens = setInterval(buscarMensagens,3000);
+}
+
+//Em caso de nome inválido ou existente
 const nomeExistente = erro => {
-    switch (erro.response.status) {
-        case 400:
-            novoNome = prompt("Nome já existente. Digite outro nome: ");
-            adicionarNovoParticipante();
-            break;
+
+    if (erro.response.status === 400) {
+        nomeParticipante = prompt("Este nome já está em uso. Digite outro nome:");
     }
+    adicionarParticipante();
 }
 
-//mantém conexão do usuario enviando o objeto para a API a cada 5s.
-//chama função a cada de buscar e atualizar mensagens
-const participantesOnline = () => {
+//Enviar status do partipante a cada 5s
+const enviarStatus = () => {
 
-    const usuariosOnline = {
-        name: novoNome
+    const participante = {
+        name: nomeParticipante
     }
 
-    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/status/36779bab-ebca-4960-aa5c-78095c04c3f6", usuariosOnline);
-    promessa.then(atualizarMensagens);
-    promessa.catch(participanteOffline);
+    const promessa = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/status/2859eeff-8821-45c5-ab78-67cda96705d2",
+        participante
+    );
+    promessa.then(retornoSucesso);
+    promessa.catch(retornoErro);
 
 }
 
-const atualizarParticipantes = () => setInterval(participantesOnline, 5000);
+const retornoSucesso = resposta => console.log(resposta);
+const retornoErro = erro => console.log(erro);
 
-//Faz um get na API e tras as mensagens e chama função de renderizar msgs e atualiza a cada 3s
+//Buscar mensagens do servidor
 const buscarMensagens = () => {
-    const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages/36779bab-ebca-4960-aa5c-78095c04c3f6");
-    promessa.then(renderizarMensagens);
+    const promessa = axios.get(
+        "https://mock-api.driven.com.br/api/v6/uol/messages/2859eeff-8821-45c5-ab78-67cda96705d2"
+    );
+    promessa.then(retornoSucesso);
+    promessa.catch(retornoErro);
 }
 
-const renderizarMensagens = resposta => {
-    mensagens = resposta.data;
-
-    const div = document.querySelector("main");
-    div.innerHTML = "";
-
-    for (let i = 0; i < mensagens.length; i++) {
-        if (mensagens[i].type === "message") {
-            div.innerHTML += `
-                <p class="${mensagens[i].type}">
-                ${mensagens[i].time} ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
-                <p>
-            `
-
-        }
-        else {
-            div.innerHTML += `
-                <p class="${mensagens[i].type}">
-                ${mensagens[i].time} ${mensagens[i].from} ${mensagens[i].text}
-                <p>
-            `
-        }
-    }
-
-}
-
-const atualizarMensagens = () => setInterval(buscarMensagens, 3000);
-
-
-
-const participanteOffline = erro => {
-    console.log(erro);
-}
-
-
-/* const buscarParticipantes = () => {
-    const 
-} */
-
-//funções que inicializam quando abre a página
-adicionarNovoParticipante();
-atualizarParticipantes();
-
+//executar funcao ao abrir pagina
+adicionarParticipante();
